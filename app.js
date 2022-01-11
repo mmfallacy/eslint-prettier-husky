cad1d59f582f8c55442cc5f74df777fd9d6a4244
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2022-01-12 02:30:59 +0800

    Test

---
+++ app.js
@@ -0,0 +1,24 @@
+/**
+ * The code below does not adhere to the recommended styles of ESLint and Prettier, specifically:
+ * noUnusedVars
+ * 4 spaced indents
+ * Single quotes for strings
+ */
+
+// Before commit
+
+// This must be commented out so noUnusedVars does not get triggered
+// const unusedVariable = "test";
+// function test () {
+// console.log("123")
+// }
+// test()
+
+// After commit
+
+// This must be commented out so noUnusedVars does not get triggered
+// const unusedVariable = "test";
+function test() {
+    console.log('123');
+}
+test();
